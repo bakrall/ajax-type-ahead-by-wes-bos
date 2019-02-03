bc474760f52de22e861ecce7fe642212c2a7a8ba
Barbara Stadnik <barbara1stadnik@gmail.com>
2019-02-03 18:36:52 +0100

    refactor code

--- search.js
+++ search.js
@@ -1,6 +1,7 @@
-const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
-
-const cities = [];
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json',
+    cities = [],
+	searchInput = document.querySelector('.search'),
+	suggestions = document.querySelector('.suggestions');
 
 //fetch returns a promise
 //it does not know what kind of data comes from fetch
@@ -25,10 +26,11 @@ function numberWithCommas(x) {
 
 function displayMatches() {
 	const matchArray = findMatches(this.value, cities);
+
 	const suggestionsListHtml = matchArray.map(place => {
-		const regex = new RegExp(this.value, 'gi');
-		const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
-		const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+		const regex = new RegExp(this.value, 'gi'),
+		    cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`),
+		    stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
 
 		return `
 			<li>
@@ -40,9 +42,6 @@ function displayMatches() {
 
 	suggestions.innerHTML = suggestionsListHtml;
 }
-
-const searchInput = document.querySelector('.search');
-const suggestions = document.querySelector('.suggestions');
 
 //listen for two types of events on one element
 ['keyup', 'change'].forEach(event => searchInput.addEventListener(event, displayMatches));
